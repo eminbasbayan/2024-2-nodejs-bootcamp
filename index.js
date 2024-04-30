8a340407921b605fe5347f41852c6d47e26ef4ef
eminbasbayan <emin.basbayan@icloud.com>
2024-04-30 23:32:28 +0300

    module scope

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-const add = require("./add");
-const addFunction = require("./add");
-
-const sum = add(10, 40);
-const sum2 = addFunction(10, 55);
-console.log(sum);
-console.log(sum2);
+require("./batman.js");
+require("./superman.js");
