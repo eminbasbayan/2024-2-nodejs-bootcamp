dada34c2299a221cbe4ec00ee1021dc85a875382
eminbasbayan <emin.basbayan@icloud.com>
2024-04-30 22:52:06 +0300

    local modules

---
+++ add.js
@@ -0,0 +1,7 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const sum = add(4, 8);
+
+console.log(sum);
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("Hello World!");
+require("./add")
+
+// console.log("Hello World!");
