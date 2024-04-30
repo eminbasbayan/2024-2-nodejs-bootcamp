3c20089e2ed807ebfdee4ea9434c24ba6ce41cf2
eminbasbayan <emin.basbayan@icloud.com>
2024-04-30 23:00:32 +0300

    module exports

--- add.js
+++ add.js
@@ -2,6 +2,4 @@ const add = (a, b) => {
   return a + b;
 };
 
-const sum = add(4, 8);
-
-console.log(sum);
+module.exports = add;
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-require("./add")
+const add = require("./add");
+const addFunction = require("./add");
 
-// console.log("Hello World!");
+const sum = add(10, 40);
+const sum2 = addFunction(10, 55);
+console.log(sum);
+console.log(sum2);
