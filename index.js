d180eec143056ddbf5f93a569c44cee764801c7f
eminbasbayan <emin.basbayan@icloud.com>
2024-05-06 21:18:56 +0300

    callback pattern

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-const path = require("node:path");
+function helloFn(name) {
+  console.log(`Hello, ${name}`);
+}
 
-// console.log(path.basename(__filename));
-// console.log(path.basename(__dirname));
+function helloName(callbackFn) {
+  const name = "Emin";
+  callbackFn(name);
+}
 
-// console.log(path.extname(__filename));
-// console.log(path.extname(__dirname));
-
-// console.log(path.parse(__filename));
-// console.log(path.format(path.parse(__filename)));
-
-// console.log(__filename);
-// console.log(__dirname);
-
-// console.log(
-//   path.join(__dirname, "folder1", "folder2", "folder3", "index.html")
-// );
-
-console.log(path.join("/folder1", "folder2", "folder3", "index.html"));
-console.log(path.join("/folder1", "//folder2", "folder3", "index.html"));
-console.log(path.join("/folder1", "//folder2", "folder3", "../index.html"));
+helloName(helloFn);
