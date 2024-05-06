522e4bd190d624ab698c9bccb908e1931387bc15
eminbasbayan <emin.basbayan@icloud.com>
2024-05-06 20:40:36 +0300

    import JSON ve watch mode

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-/* const superHero = require("./super-hero.js");
+const data = require("./data.json");
 
-console.log(superHero.getName()); // Batman
+console.log(data);
 
-superHero.setName("Superman");
-console.log(superHero.getName()); // Superman
-
-const newSuperHero = require("./super-hero.js");
-console.log(newSuperHero.getName()); // Batman
- */
-
-const SuperHero = require("./super-hero.js");
-const superman = new SuperHero("Superman")
-console.log(superman.getName());
-const batman = new SuperHero("Batman")
-console.log(batman.getName());
+// node --watch index.js
