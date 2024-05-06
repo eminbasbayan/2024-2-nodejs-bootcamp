015085f3d9347594b43a221ca0cc79044ad38b28
eminbasbayan <emin.basbayan@icloud.com>
2024-05-06 21:12:07 +0300

    path module

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const data = require("./data.json");
+const path = require("node:path");
 
-console.log(data);
+// console.log(path.basename(__filename));
+// console.log(path.basename(__dirname));
 
-// node --watch index.js
+// console.log(path.extname(__filename));
+// console.log(path.extname(__dirname));
+
+// console.log(path.parse(__filename));
+// console.log(path.format(path.parse(__filename)));
+
+// console.log(__filename);
+// console.log(__dirname);
+
+// console.log(
+//   path.join(__dirname, "folder1", "folder2", "folder3", "index.html")
+// );
+
+console.log(path.join("/folder1", "folder2", "folder3", "index.html"));
+console.log(path.join("/folder1", "//folder2", "folder3", "index.html"));
+console.log(path.join("/folder1", "//folder2", "folder3", "../index.html"));
