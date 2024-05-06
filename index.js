e12a3863ed34f950b0f6adb7881ef652f0eaea01
eminbasbayan <emin.basbayan@icloud.com>
2024-05-06 23:58:51 +0300

    fs module

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
-const PizzaShop = require("./pizza-shop.js");
-const DrinkMachine = require("./drink-machine.js");
+const fs = require("node:fs");
 
-const pizzaShop = new PizzaShop();
-const drinkMachine = new DrinkMachine();
+//! Senkron dosya okuma
+// const txtFile = fs.readFileSync("./file.txt", "utf-8");
+// console.log(txtFile.toString());
 
-pizzaShop.on("orderPizza", (size, topping, quantity) => {
-  console.log(
-    `${quantity} adet ${size} boyutunda pizza sipariş verildi. İçindekiler: ${topping}`
-  );
-  drinkMachine.serveDrink(size);
+//! Asenkron dosya okuma
+// fs.readFile("./file.txt", (err, data) => {
+//   setTimeout(() => {
+//     if (err) {
+//       console.log("Dosya okunurken bir hata oluştu:", err);
+//     } else {
+//       console.log(data.toString());
+//     }
+//   }, 3000);
+// });
+// console.log("async");
+
+//! Senkron dosya içine yazı yazma işlemi
+// fs.writeFileSync("./file2.txt", "Hello World!");
+
+//! Asenkron dosya içine yazı yazma işlemi
+// fs.writeFile("./file3.txt", "Emin Başbayan", (err) => {
+//   if (err) {
+//     console.log("Dosya işleminde bir hata oluştu:", err);
+//   }else{
+//     console.log("Dosya işlemi başarılı!");
+//   }
+// });
+
+//! Dosya içine yazı ekleme (asenkron)
+// fs.writeFile("./file3.txt", "BilGen Yazılım Akademi", { flag: "a" }, (err) => {
+//   if (err) {
+//     console.log("Dosya işleminde bir hata oluştu:", err);
+//   } else {
+//     console.log("Dosya işlemi başarılı!");
+//   }
+// });
+
+//! Dosya silme işlemi
+fs.unlink("./file3.txt", (err) => {
+  if (err) {
+    console.log("Dosya silinemedi:", err);
+    return;
+  }
+  console.log("Dosya başarıyla silindi!");
 });
-
-pizzaShop.order("large", "mantar", 5);
-pizzaShop.order("small", "mısır", 2);
-pizzaShop.displayOrderNumber();
