8c685df9858d38eb051c8f3289f2a69a7f325ca0
eminbasbayan <emin.basbayan@icloud.com>
2024-05-07 21:19:22 +0300

    fs promises module

--- index.js
+++ index.js
@@ -1,47 +1,27 @@
-const fs = require("node:fs");
+const fs = require("node:fs").promises;
+// const fs = require("node:fs/promises");
 
-//! Senkron dosya okuma
-// const txtFile = fs.readFileSync("./file.txt", "utf-8");
-// console.log(txtFile.toString());
+// fs.readFile("./file.txt", "utf-8")
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err))
+//   .finally(() => console.log("işlem tamamlandı!"));
 
-//! Asenkron dosya okuma
-// fs.readFile("./file.txt", (err, data) => {
-//   setTimeout(() => {
-//     if (err) {
-//       console.log("Dosya okunurken bir hata oluştu:", err);
-//     } else {
-//       console.log(data.toString());
-//     }
-//   }, 3000);
-// });
-// console.log("async");
-
-//! Senkron dosya içine yazı yazma işlemi
-// fs.writeFileSync("./file2.txt", "Hello World!");
-
-//! Asenkron dosya içine yazı yazma işlemi
-// fs.writeFile("./file3.txt", "Emin Başbayan", (err) => {
-//   if (err) {
-//     console.log("Dosya işleminde bir hata oluştu:", err);
-//   }else{
-//     console.log("Dosya işlemi başarılı!");
-//   }
-// });
-
-//! Dosya içine yazı ekleme (asenkron)
-// fs.writeFile("./file3.txt", "BilGen Yazılım Akademi", { flag: "a" }, (err) => {
-//   if (err) {
-//     console.log("Dosya işleminde bir hata oluştu:", err);
-//   } else {
-//     console.log("Dosya işlemi başarılı!");
-//   }
-// });
-
-//! Dosya silme işlemi
-fs.unlink("./file3.txt", (err) => {
-  if (err) {
-    console.log("Dosya silinemedi:", err);
-    return;
+const readFile = async () => {
+  try {
+    const data = await fs.readFile("./file.txt");
+    console.log(data);
+    return "Emin";
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log("işlem tamamlandı");
   }
-  console.log("Dosya başarıyla silindi!");
-});
+};
+
+readFile()
+  .then((data) => {
+    return data;
+  })
+  .then((data) => console.log(data));
+
+console.log("async");
