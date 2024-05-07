d7ed975a874635c47d83c1a84727390b0b25f639
eminbasbayan <emin.basbayan@icloud.com>
2024-05-07 23:36:04 +0300

    html response

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
 const server = http.createServer((request, response) => {
   const person = {
@@ -6,8 +7,10 @@ const server = http.createServer((request, response) => {
     lastName: "Başbayan",
   };
 
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(person));
+  response.writeHead(200, { "Content-Type": "text/html" });
+  // fs.createReadStream(__dirname + "/index.html").pipe(response);
+  const html = fs.readFileSync("./index.html", "utf-8");
+  response.end(html);
 });
 
 server.listen(3000, () => {
