d797bb08bee210d662446ec10afdc6d97f0f965b
eminbasbayan <emin.basbayan@icloud.com>
2024-05-07 23:17:11 +0300

    node server oluşturma

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-// 1- fs modülünü kullanarak, belirli bir dizindeki bir dosyayı okuyun. Bu dosya, basit bir metin dosyası olabilir.
+const http = require("node:http");
 
-// 2- Dosyanın içeriğini konsola yazdırın.
-// path modülünü kullanarak, dosyanın tam yolunu (absolute path) hesaplayın ve bu yolu konsola yazdırın.
-
-// 3- Dosyanın adını ve uzantısını ayırarak konsola yazdırın.
-
-const fs = require("node:fs");
-const path = require("node:path");
-
-// Dosya yolu
-const dosyaYolu = path.join(__dirname, "file.txt");
-
-// Dosya okuma
-fs.readFile(dosyaYolu, "utf-8", (err, data) => {
-  if (err) {
-    console.log("Dosya okunurken bir hata oluştu:", hata);
-    return;
-  }
-  console.log("Dosya içeriği:", data);
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Hello World!");
 });
 
-// Tam yolu
-console.log("Tam dosya yolu:", dosyaYolu);
-
-// Dosya adı ve uzantısı
-console.log("Dosya adı:", path.basename(dosyaYolu));
-console.log("Dosya uzantısı:", path.extname(dosyaYolu));
-
+server.listen(3000, () => {
+  console.log("Server runnig on port 3000");
+});
