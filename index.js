8c717128600e9f4f5424487a80672acb906b5fc4
eminbasbayan <emin.basbayan@icloud.com>
2024-05-08 00:01:32 +0300

    http routing

--- index.js
+++ index.js
@@ -2,14 +2,31 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((request, response) => {
-  const name = "Arda";
-
-  response.writeHead(200, { "Content-Type": "text/html" });
-  let html = fs.readFileSync("./index.html", "utf-8");
-  html = html.replace("{{name}}", name);
-  response.end(html);
+  if (request.url === "/") {
+    const name = "Arda";
+    response.writeHead(200, { "Content-Type": "text/html" });
+    let html = fs.readFileSync("./index.html", "utf-8");
+    html = html.replace("{{name}}", name);
+    response.end(html);
+  } else if (request.url === "/products") {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Products Page");
+  } else if (request.url === "/api") {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(
+      JSON.stringify({
+        firsName: "Emin",
+        lastName: "Başbayan",
+      })
+    );
+  } else {
+    response.writeHead(404);
+    response.end("Page not found!");
+  }
 });
 
-server.listen(3000, () => {
-  console.log("Server runnig on port 3000");
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server runnig on port ${PORT}`);
 });
