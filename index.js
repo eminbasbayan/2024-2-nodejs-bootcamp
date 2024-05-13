78f5974bca1f31299f5929b902fc9d51f42515d5
eminbasbayan <emin.basbayan@icloud.com>
2024-05-13 22:59:27 +0300

    express kurulumu

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-const http = require("node:http");
-const fs = require("node:fs");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const server = http.createServer((request, response) => {
-  if (request.url === "/") {
-    const name = "Arda";
-    response.writeHead(200, { "Content-Type": "text/html" });
-    let html = fs.readFileSync("./index.html", "utf-8");
-    html = html.replace("{{name}}", name);
-    response.end(html);
-  } else if (request.url === "/products") {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Products Page");
-  } else if (request.url === "/api") {
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.end(
-      JSON.stringify({
-        firsName: "Emin",
-        lastName: "Başbayan",
-      })
-    );
-  } else {
-    response.writeHead(404);
-    response.end("Page not found!");
-  }
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-const PORT = 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server runnig on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server runnig on port ${port}`);
 });
