4bf548dc374b7f4e6005278796452a9a9eddd520
eminbasbayan <emin.basbayan@icloud.com>
2024-05-13 23:41:41 +0300

    res redirect

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.get("/products", (req, res) => {
   res.status(200).sendFile(path.join(__dirname, "views", "products.html"));
 });
 
+app.get("/new-page.html", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+
+app.get("/old-page", (req, res) => {
+  res.redirect(301, "/new-page.html");
+});
+
 app.get("/api/customers", (req, res) => {
   res.status(200).json([
     {
