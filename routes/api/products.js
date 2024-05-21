20e65136df4e612132b0a862714760d1e72e6ff4
eminbasbayan <emin.basbayan@icloud.com>
2024-05-21 23:14:14 +0300

    products controller oluşturma

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,37 +1,20 @@
 const express = require("express");
+const {
+  getAllProducts,
+  createNewProduct,
+  updateProduct,
+  getProductById,
+  deleteProduct,
+} = require("../../controllers/productsController");
 const router = express.Router();
-const data = {};
-data.products = require("../../data/products.json");
 
 router
   .route("/")
-  .get((req, res) => {
-    res.json(data.products);
-  })
-  .post((req, res) => {
-    res.json({
-      productName: req.body.productName,
-      categoryName: req.body.categoryName,
-    });
-  })
-  .put((req, res) => {
-    res.json({
-      productName: req.body.productName,
-      categoryName: req.body.categoryName,
-    });
-  })
-  .delete((req, res) => {
-    res.json({
-      id: req.body.id,
-    });
-  });
+  .get(getAllProducts)
+  .post(createNewProduct)
+  .put(updateProduct)
+  .delete(deleteProduct);
 
-router.route("/:productId").get((req, res) => {
-  const productId = req.params.productId;
-  const findProduct = data.products.find(
-    (item) => item.id === Number(productId)
-  );
-  res.json(findProduct);
-});
+router.route("/:productId").get(getProductById);
 
 module.exports = router;
