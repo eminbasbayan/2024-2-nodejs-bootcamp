45c6c1adddae3e998aba33fbac094814f956f8dc
eminbasbayan <emin.basbayan@icloud.com>
2024-06-25 23:11:54 +0300

    network i/o

--- libuv.js
+++ libuv.js
@@ -1,38 +1,18 @@
-/* const fs = require("node:fs");
+const https = require("node:https");
 
-console.log("Başlangıç");
-
-fs.readFile("file.txt", (err, data) => {
-  if (err) throw err;
-  console.log(data.toString());
-});
-
-console.log("Bitiş"); */
-
-/* ********************* 1991 ms */
-
-// const crypto = require("node:crypto");
-
-// const start = Date.now();
-
-// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
-// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
-// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
-
-// console.log(`Süre: ${Date.now() - start} ms`);
-
-/* ******************************** */
-
-const crypto = require("node:crypto");
-
-// process.env.UV_THREADPOOL_SIZE = 6;
+const CALL_LIMIT = 30;
 
 const start = Date.now();
 
-const CALL_LIMIT = 12;
-
 for (let i = 0; i < CALL_LIMIT; i++) {
-  crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
-    console.log(`Süre: ${Date.now() - start} ms`);
-  });
+  https
+    .request("https://google.com", (res) => {
+      res.on("data", () => {});
+      res.on("end", () => {
+        console.log(`Request: ${i + 1}: ${Date.now() - start} ms`);
+      });
+    })
+    .end();
 }
+
+console.log("bitiş");
