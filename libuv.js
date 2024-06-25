bc9a466f453597b74e398231e2dc067cfdd4d3c1
eminbasbayan <emin.basbayan@icloud.com>
2024-06-25 21:55:42 +0300

    thread pool

--- libuv.js
+++ libuv.js
@@ -1,11 +1,36 @@
-/* console.log("başlangıç");
-for (let i = 0; i < 9000000000; i++) {}
-console.log("bitiş"); */
+/* const fs = require("node:fs");
 
-/* ************ */
+console.log("Başlangıç");
 
-console.log("başlangıç");
-setTimeout(() => {
-  for (let i = 0; i < 9000000000; i++) {}
-}, 1000);
-console.log("bitiş");
+fs.readFile("file.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
+
+console.log("Bitiş"); */
+
+/* ********************* 1991 ms */
+
+// const crypto = require("node:crypto");
+
+// const start = Date.now();
+
+// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+
+// console.log(`Süre: ${Date.now() - start} ms`);
+
+/* ******************************** */
+
+const crypto = require("node:crypto");
+
+const start = Date.now();
+
+const CALL_LIMIT = 3;
+
+for (let i = 0; i < CALL_LIMIT; i++) {
+  crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
+    console.log(`Süre: ${Date.now() - start} ms`);
+  });
+}
